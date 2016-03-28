8615f0c13aa2fea86c219a63503bee05d834f3ff
Zach Macfarlane <Zach Macfarlane>
2016-03-28 07:47:45 +0100

    Improved appearance, and added baseline document browser

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,15 @@ router.get('/about', function(req, res, next) {
 
 /* GET browse documents page. */
 router.get('/browse', function(req, res, next) {
-  res.render('browse', { title: 'Browse the Colenso Project' });
+  client.execute("XQUERY db:list('Colenso')",
+  function (error, result) {
+    if(error){console.error(error);}
+    else{
+      list = result.result.split("\r\n");
+      res.render('browse', { title: 'Browse Documents', list: list });
+    }
+  }
+);
 });
 
 /* GET search page. */
@@ -25,42 +33,7 @@ router.get('/search', function(req, res, next) {
 });
 
 router.get('/searchresults', function(req, res) {
-  res.render('search_results', { title: 'My Movie Store', content: req.query.searchString });
+  res.render('search_results', { title: 'Search the Colenso Project', content: req.query.searchString });
 });
 
 module.exports = router;
-/*
-router.get("/search/results", function(req, res){
-  client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-  " (//name[@type='place'])[1 to 10] ",
-  function (error, result) {
-    if(error){ console.error(error);}
-    else {
-      res.render('searchResults', {results: result.result});
-    }
-  });
-});
-*/
-/*
-router.get("/",function(req,res){
-client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-" (//name[@type='place'])[1] ",
-function (error, result) {
-if(error){ console.error(error);}
-else {
-res.render('index', { title: 'ECS Video Rental', place: result.result });
-}
-}
-);
-});
-*/
-/*
-router.get('/search/results', function(req, res) {
-  var queries = req.query;
-  client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0'; " +
-  "//name[@type = 'place' and position() = 1 and . = '” + queries.mysearch + ”']",
-  function(err,res) { if(!err) console.log(res.result)} )
-  res.render('search_results', { title: 'Search Results', content: queries.searchString});
-  //, content: req.query.searchString });
-});
-*/
