28c35ea96bd83a6b7e4cd3c24efb5696a3ec077c
Zach Macfarlane <zachmacfarlane@live.com>
2016-03-30 05:01:35 +0100

    Added search funtionality

--- routes/index.js
+++ routes/index.js
@@ -31,9 +31,22 @@ router.get('/browse', function(req, res, next) {
 router.get('/search', function(req, res, next) {
   res.render('search', { title: 'Search the Colenso Project' });
 });
-
+/*
 router.get('/searchresults', function(req, res) {
   res.render('search_results', { title: 'Search the Colenso Project', content: req.query.searchString });
 });
-
+*/
+router.get('/searchresults', function(req, res) {
+  client.execute("XQUERY declare namespace tei = 'http://www.tei-c.org/ns/1.0'; " +
+  //"//tei:name[@type = 'place' and position() = 1 and . ='” + req.query.searchString + ”']",
+  "for $p in *[.//text() contains text '" + req.query.searchString + "'] return db:path($p)",
+  function(err,result) {
+    if(err){console.error(err);}
+    else{
+      list = result.result.split("\r\n");
+      numResults = list.length;
+      //res.render('search_results', { title: 'Search the Colenso Project', content: req.query.searchString, result: res.result });
+      res.render('search_results', { title: 'Search the Colenso Project', content: req.query.searchString, list: list, numResults: numResults});
+    }});
+});
 module.exports = router;
